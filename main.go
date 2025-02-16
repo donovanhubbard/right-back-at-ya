dba0dbb35e3440cd816e319aed1d2e7c6b6ee21f
Donovan Hubbard <37090676+donovanhubbard@users.noreply.github.com>
2025-02-16 08:18:59 -0600

    Completed diagnostic information returned to user

--- main.go
+++ main.go
@@ -1,45 +1,81 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"io/ioutil"
+	"io"
+	"log/slog"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
+func getStatusCode(url string) (int, error) {
+	trimmedUrl := strings.TrimPrefix(url, "/")
+	status, err := strconv.Atoi(trimmedUrl)
+
+	if err != nil {
+		return 0, errors.New("Not a valid integer code")
+	}
+
+	if status >= 100 && status <= 600 {
+		return status, nil
+	}
+
+	return 0, errors.New("Not a valid status code")
+}
+
 func rbay(w http.ResponseWriter, r *http.Request) {
+	var returnCode int
+	status, error := getStatusCode(r.URL.String())
+
+	if error != nil {
+		returnCode = 200
+	} else {
+		returnCode = status
+	}
+
 	fmt.Fprintf(w, "Method: %s\n", r.Method)
 	fmt.Fprintf(w, "URL: %v\n", r.URL)
 	fmt.Fprintf(w, "Protocol: %s\n", r.Proto)
 	fmt.Fprintf(w, "Host: %s\n", r.Host)
+	fmt.Fprintf(w, "Remote Address: %s\n", r.RemoteAddr)
+
 	fmt.Fprintf(w, "Headers:\n")
 
 	for key := range r.Header {
 		fmt.Fprintf(w, "  %s: %s\n", key, r.Header[key])
 	}
 
-	body, err := ioutil.ReadAll(r.Body)
+	body, err := io.ReadAll(r.Body)
 	defer r.Body.Close()
 	if err != nil {
 		fmt.Println("Not able to read body. ", err)
 	}
+	stringBody := string(body)
+	if len(stringBody) > 0 {
+		fmt.Fprintf(w, "Body:\n")
+		fmt.Fprintf(w, "  %v\n", string(body))
+	}
 
-	fmt.Fprintf(w, "Body:\n")
-	fmt.Fprintf(w, "  %v\n", string(body))
-	fmt.Fprintf(w, "Remote Address: %s\n", r.RemoteAddr)
-	fmt.Fprintf(w, "Cookies:\n")
+	if len(r.Cookies()) > 0 {
+		fmt.Fprintf(w, "Cookies:\n")
 
-	// for c := range r.Cookies() {
-	// 	fmt.Fprintf(w, "  %v", c)
-	// 	// fmt.Fprintf(w, "  Name:%s", c.Name)
-	// 	// fmt.Fprintf(w, "  Value:%s", c.Value)
-	// 	// fmt.Fprintf(w, "  Path:%s", c.Path)
-	// }
+		for _, c := range r.Cookies() {
+			fmt.Fprintf(w, "  Name: %s\n", c.Name)
+			fmt.Fprintf(w, "  Value: %s\n", c.Value)
+			fmt.Fprintf(w, "  Path: %s\n\n", c.Path)
+		}
+	}
+
+	fmt.Fprintf(w, "HTTP Status code: %d\n", returnCode)
+
+	slog.Info(fmt.Sprintf("%s %d %s %s", r.RemoteAddr, returnCode, r.Method, r.URL))
 }
 
 func main() {
-	fmt.Println("Starting")
-	mux := &http.ServeMux{}
-	mux.HandleFunc("/", rbay)
-	http.ListenAndServe(":8085", mux)
-	fmt.Println("Exiting")
+	fmt.Println("Server started.")
+	http.HandleFunc("/", rbay)
+	err := http.ListenAndServe(":8085", nil)
+	slog.Error(fmt.Sprintf("%v", err))
 }
